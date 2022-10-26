c266857cd2536193f7075026af2983cbd9f3f71c
AnkithReddyMora <moraankithreddy@gmail.com>
2022-10-26 21:17:00 +0000

    topic2

--- bottle_app.py
+++ bottle_app.py
@@ -1,63 +1,40 @@
-
-# A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app, route, get, post, template, request, redirect
-import sqlite3
 
-connection = sqlite3.connect("shopping_list.db")
+import database
 
 @route('/')
-def hello_world():
-    return 'Hello from Ankith!'
-
-@route('/bye')
-def bye_world():
-    return 'Bye from Ankith!'
+def get_index():
+    redirect('/list')
 
 @route('/list')
 def get_list():
-    cursor = connection.cursor()
-    rows = cursor.execute("select id, description from list")
-    rows = list(rows)
-    rows = [ {'id':row[0] ,'desc':row[1]} for row in rows ]
-    return template("shopping_list.tpl", name = "Ankith", shopping_list=rows)
-
-@get('/add')
-def get_add():
-    return template("add_item.tpl")
+    items = database.get_items()
+    return template("shopping_list.tpl", name="Dr. DeLozier", shopping_list=items)
 
 @post('/add')
 def post_add():
-    cursor = connection.cursor()
-    cursor.execute(f"insert into list (description) values ('{description}')")
-    connection.commit()
+    description = request.forms.get("description")
+    database.add_item(description)
     redirect('/list')
 
-@route('/delete/<id>')
+@route("/delete/<id>")
 def get_delete(id):
-    cursor = connection.cursor()
-    cursor.execute(f"delete from list where id={id}")
-    connection.commit()
+    database.delete_item(id)
     redirect('/list')
 
-@get('/edit/<id>')
+@get("/edit/<id>")
 def get_edit(id):
-    cursor = connection.cursor()
-    items = cursor.execute(f"select description from list where id={id}")
-    items = list(items)
-    if len(items)!=1:
+    items = database.get_items(id)
+    if len(items) != 1:
         redirect('/list')
-    description = items[0][0]
+    item_id, description = items[0]['id'], items[0]['desc']
+    assert item_id == int(id)
     return template("edit_item.tpl", id=id, description=description)
-    return f"Editing {description} for item {id}"
 
 @post("/edit/<id>")
 def post_edit(id):
     description = request.forms.get("description")
-    cursor = connection.cursor()
-    cursor.execute(f"update list set description='{description}' where id={id}")
-    connection.commit()
+    database.update_item(id, description)
     redirect('/list')
 
-
 application = default_app()
-
---
+++ views/setup.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect("shopping_list.db")
+
+cursor = connection.cursor()
+try:
+     cursor.execute("drop table list")
+except:
+    pass
+
+cursor.execute("create table list (id integer primary key, description text)")
+
+cursor.execute("insert into list (description) values ('Maggie')")
+cursor.execute("insert into list (description) values ('chicken')")
+cursor.execute("insert into list (description) values('almond joy')")
+cursor.execute("insert into list (description) values ('danon')")
+cursor.execute("insert into list (description) values ('bread')")
+connection.commit()
+connection.close()
